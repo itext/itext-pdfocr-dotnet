ab531f99d9a087ebb1f8fc5ed6c38c0a4fb02f57
iText Software <development@itextpdf.com>
2022-12-02 12:30:52 +0000

    [AFTER RELEASE] pdfOCR 3.0.0

--- itext.tests/itext.pdfocr.api.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.pdfocr.api.tests/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("d6a6ea97-1f23-448f-b700-eff62971d234")]
 
-[assembly: AssemblyVersion("2.0.3.0")]
-[assembly: AssemblyFileVersion("2.0.3.0")]
-[assembly: AssemblyInformationalVersion("2.0.3-SNAPSHOT")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-SNAPSHOT")]
