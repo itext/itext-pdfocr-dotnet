1e1eeaf59f053f869116d1a50ac2cf449b2eed10
Apryse Software <development@apryse.com>
2023-10-17 15:31:14 +0000

    [RELEASE] pdfOCR 3.0.1

--- itext/itext.pdfocr.api/Properties/AssemblyInfo.cs
+++ itext/itext.pdfocr.api/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("3.0.1.0")]
 [assembly: AssemblyFileVersion("3.0.1.0")]
-[assembly: AssemblyInformationalVersion("3.0.1-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("3.0.1")]
 [assembly: InternalsVisibleTo("itext.pdfocr.api.tests, PublicKey=" +
     "00240000048000009400000006020000002400005253413100040000010001008b21ed5b3fc1c1" +
     "1996390981fe22bbe71a39a9e11d3c2cefddd6ee92920fa871f9666ae0fa941af0280d0653df04" +
